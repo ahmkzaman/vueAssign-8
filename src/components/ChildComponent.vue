4772010912d67990f30e046f89dd36219d7259e8
ahmkzaman <kzaman.ahm@gmail.com>
2023-08-20 13:26:26 +0600

    Vue assgin-8

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+import ParentComponent from './ParentComponent.vue';
+
+const emit = defineEmits(['messageEmitted'])
+const displayMessage = ref('Message from the Parent Component')
+
+function sendMessage() {
+    displayMessage.value = 'Message from Child Component on clicked'
+    return displayMessage.value;
+}
+
+</script>
+
+<template>
+    <div class="text-red-500">
+    <ParentComponent :message="displayMessage"/>
+    </div>
+<button @click="$emit('messageEmitted', sendMessage())" class="mt-6 hover:bg-rose-500 font-bold bg-rose-200">Update Message</button>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+defineProps({
+    message: String
+})
+
+
+</script>
+
+<template>
+<div class="container">
+    <h1>{{ message }}</h1>
+    
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
